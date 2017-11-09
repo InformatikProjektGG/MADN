9b811da72aa61c7e4e1504093d09eb672af5f207
AlexanderKoch-Koch <alexander@4koch.de>
2017-11-09 12:36:57 +0100

    added Psitions to decideActions parameters

--- src/mypackage/KI.java
+++ src/mypackage/KI.java
@@ -9,9 +9,10 @@ public class KI {
     /**
      * 
      * @param actions erlaubte Aktionen fuer die KI
+     * @param positions Positionen aller Figuren
      * @return Figur die bewegt werden soll 
      */
-    public static int decideAction(Actions actions){
+    public static int decideAction(Actions actions, Positions positions){
         for (int i = 0; i < 4; i++){
             if(actions.figurZiehen[i] > 0){
                 return i;
